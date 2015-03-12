8825ae8bf80ff6ddbe8d48329fc9d9d639804c05
zhujun87654321 <zhujun87654321@gmail.com>
2015-03-12 18:40:34 +0800

    ok

--- js/clock.js
+++ js/clock.js
@@ -2,11 +2,10 @@ window.onload = function() {
 	var sec = document.getElementById('sec');
 	var min = document.getElementById('min');
 	var hour = document.getElementById('hour');
-	
-	var deg = new Array();
-    for (var i = 0; i < 360; i++) {
-		deg[i] = 'rotate(' + i + 'deg)';
-	};
+
+	function deg(i) {
+		return 'rotate(' + i + 'deg)';
+	}
 
 	function go() {
 		var myDate = new Date();
@@ -15,16 +14,11 @@ window.onload = function() {
 		var h = myDate.getHours();
 	    var iSec = 6*s;
 	    var iMin = m*6 + parseInt(s/10);
-	    var iHour = (h%12)*30 + parseInt(m/2);//获取的时间为24h制
+	    var iHour = (h%12)*30 + parseInt(m/2)+ parseInt(s/120);
 
-		sec.style.webkitTransform = deg[iSec];
-		sec.style.transform = deg[iSec];
-
-		min.style.webkitTransform = deg[iMin];
-		min.style.transform = deg[iMin];
-		
-		hour.style.webkitTransform = deg[iHour];
-		hour.style.transform = deg[iHour];
+		sec.style.transform = deg(iSec);
+		min.style.transform = deg(iMin);
+		hour.style.transform = deg(iHour);
 	}
 	go();
 	setInterval(go,1000);
